458be8e001dcff766f23b06e63bc6856452c874a
surume <mail@sururne.com>
2015-09-20 01:01:32 +0900

    Initial Xcode project

---
+++ EarAge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarAge
+//
+//  Created by miyamoto shota on 2015/09/20.
+//  Copyright © 2015年 surume. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
