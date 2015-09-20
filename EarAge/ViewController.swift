dc887d93cbb319594d605e67cb97d4ff0a85a377
surume <mail@sururne.com>
2015-09-20 22:08:09 +0900

    Add Button

--- EarAge/ViewController.swift
+++ EarAge/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,AVAudioPlayerDelegate  {
+    
+    //変数宣言.
+    var myAudioPlayer : AVAudioPlayer!
+    var myButton : UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //再生する音源のURLを生成.
+        let soundFilePath : NSString = NSBundle.mainBundle().pathForResource("15khz", ofType: "mp3")!
+        let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath as String)
+        
+        //AVAudioPlayerのインスタンス化.
+        myAudioPlayer = try! AVAudioPlayer(contentsOfURL: fileURL, fileTypeHint: nil)
+        
+        //AVAudioPlayerのデリゲートをセット.
+        myAudioPlayer.delegate = self
+        
+        //ボタンの生成.
+        myButton = UIButton()
+        myButton.frame.size = CGSizeMake(100, 100)
+        myButton.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+        myButton.setTitle("▶︎", forState: UIControlState.Normal)
+        myButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        myButton.backgroundColor = UIColor.cyanColor()
+        myButton.addTarget(self, action: "onClickMyButton:", forControlEvents: UIControlEvents.TouchUpInside)
+        myButton.layer.masksToBounds = true
+        myButton.layer.cornerRadius = 50.0
+        self.view.addSubview(myButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //ボタンがタップされた時に呼ばれるメソッド.
+    func onClickMyButton(sender: UIButton) {
+        
+        //playingプロパティがtrueであれば音源再生中.
+        if myAudioPlayer.playing == true {
+            
+            //myAudioPlayerを一時停止.
+            myAudioPlayer.pause()
+            sender.setTitle("▶︎", forState: .Normal)
+        } else {
+            
+            //myAudioPlayerの再生.
+            myAudioPlayer.play()
+            sender.setTitle("■", forState: .Normal)
+        }
     }
-
-
+    
+    //音楽再生が成功した時に呼ばれるメソッド.
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        print("Music Finish")
+        
+        //再度myButtonを"▶︎"に設定.
+        myButton.setTitle("▶︎", forState: .Normal)
+    }
+    
+    //デコード中にエラーが起きた時に呼ばれるメソッド.
+    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer, error: NSError?) {
+        print("Error")
+    }
+    
+    //
+    //    override func didReceiveMemoryWarning() {
+    //        super.didReceiveMemoryWarning()
+    //        // Dispose of any resources that can be recreated.
+    //    }
+    
+    
 }
 
