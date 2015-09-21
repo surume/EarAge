86091262fad418920837e58170b03e089339aa63
surume <mail@sururne.com>
2015-09-21 22:54:34 +0900

    comment in

--- EarAge/ViewController.swift
+++ EarAge/ViewController.swift
@@ -71,11 +71,11 @@ class ViewController: UIViewController,AVAudioPlayerDelegate  {
         print("Error")
     }
     
-    //
-    //    override func didReceiveMemoryWarning() {
-    //        super.didReceiveMemoryWarning()
-    //        // Dispose of any resources that can be recreated.
-    //    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     
 }
