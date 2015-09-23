4600afb119cc2e0abfdc594bfecbcc1e0e66d311
surume <mail@sururne.com>
2015-09-23 19:32:05 +0900

    Reformat

--- EarAge/ViewController.swift
+++ EarAge/ViewController.swift
@@ -10,24 +10,24 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController,AVAudioPlayerDelegate  {
-    
+
     //変数宣言.
     var myAudioPlayer : AVAudioPlayer!
     var myButton : UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         //再生する音源のURLを生成.
         let soundFilePath : NSString = NSBundle.mainBundle().pathForResource("15khz", ofType: "mp3")!
         let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath as String)
-        
+
         //AVAudioPlayerのインスタンス化.
         myAudioPlayer = try! AVAudioPlayer(contentsOfURL: fileURL, fileTypeHint: nil)
-        
+
         //AVAudioPlayerのデリゲートをセット.
         myAudioPlayer.delegate = self
-        
+
         //ボタンの生成.
         myButton = UIButton()
         myButton.frame.size = CGSizeMake(100, 100)
@@ -40,43 +40,39 @@ class ViewController: UIViewController,AVAudioPlayerDelegate  {
         myButton.layer.cornerRadius = 50.0
         self.view.addSubview(myButton)
     }
-    
+
     //ボタンがタップされた時に呼ばれるメソッド.
     func onClickMyButton(sender: UIButton) {
-        
+
         //playingプロパティがtrueであれば音源再生中.
         if myAudioPlayer.playing == true {
-            
+
             //myAudioPlayerを一時停止.
             myAudioPlayer.pause()
             sender.setTitle("▶︎", forState: .Normal)
         } else {
-            
+
             //myAudioPlayerの再生.
             myAudioPlayer.play()
             sender.setTitle("■", forState: .Normal)
         }
     }
-    
+
     //音楽再生が成功した時に呼ばれるメソッド.
     func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
         print("Music Finish")
-        
+
         //再度myButtonを"▶︎"に設定.
         myButton.setTitle("▶︎", forState: .Normal)
     }
-    
+
     //デコード中にエラーが起きた時に呼ばれるメソッド.
     func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer, error: NSError?) {
         print("Error")
     }
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
-
